f72107f486dd3771d36a02ae2c6c2427af84d5f3
=acuicultor <ramoncuenca4@gmail.com>
2023-09-06 18:14:16 +0200

    sched/fair: Reduce latency

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -59,7 +59,7 @@ unsigned int task_fork_on_bigcluster = 0;
 
 /*
  * Targeted preemption latency for CPU-bound tasks:
- * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 5ms * (1 + ilog(ncpus)), units: nanoseconds)
  *
  * NOTE: this latency value is not the same as the concept of
  * 'timeslice length' - timeslices in CFS are of variable length
@@ -69,8 +69,8 @@ unsigned int task_fork_on_bigcluster = 0;
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 6000000ULL;
-unsigned int normalized_sysctl_sched_latency = 6000000ULL;
+unsigned int sysctl_sched_latency = 5000000ULL;
+unsigned int normalized_sysctl_sched_latency = 5000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
